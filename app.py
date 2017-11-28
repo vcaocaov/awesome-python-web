d5c46bd6ac404dad7217d8359e44e926bb2c22bf
shuaic <caoshuai354826@gmail.com>
2017-11-28 03:08:29 -0500

    modified server listening ip

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from aiohttp import web
 
 
 def index(request):
-    return web.Response(body=b'<h1>Awesone<h1>')
+    return web.Response(body='<h1>Awesone<h1>'
 
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    srv = yield from loop.create_server(app.make_handler(), '0.0.0.0', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
